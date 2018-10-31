e480bd7d7f7e950164311503b739f1a25c2ecf44
Jefferson Silva <34176051+Jefferson023@users.noreply.github.com>
2018-10-31 16:18:16 -0300

    Node done

--- Tree.py
+++ Tree.py
@@ -25,5 +25,24 @@ class Node:
 		return counter
 
 class binary_tree:
-	def __init__(self):
-		self.tree = []
+	def getroot(self):
+		return self.root
+	def add(self, value, father = None):
+		if (self.root is None):
+			self.root = Node(value)
+			return None
+		if (father is None):
+			print(value)
+			return None
+		if (father.left_child is None):
+			father.left_child = Node(value, father = father, right_child = father)
+			return None
+		if ((father.right_child is None) or (father.right_child.father != father)):
+			father.right_child = Node(value, father = father, right_child = father.right_child)	
+	def remove(self, node):
+		if (node == None):
+			return None	
+		if (node == self.root):
+			self.root = None
+			gc.collect()	
+			return None
