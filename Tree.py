1f2a6aa20f4751994960151eeacf1d9cd6df3b04
Inclusão Digital <inclusaodigital@A303-05L.imd.local>
2018-10-31 12:21:27 -0300

    inicio

--- Tree.py
+++ Tree.py
@@ -1,10 +1,15 @@
 class node:
-	def __init__(self):
+	def __init__(self, value = None, left = None, right = None):
+		self.value = value
+		self.left = left
+		self.right = right
 		
 
 class binary_tree:
 	def __init__(self):
 		self.tree = []
+	def print(self):
+	
 
 
 		
