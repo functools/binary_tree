3f87064964d0cd429ba4df0a7a0b92e62dfd9cbf
Jefferson Silva <34176051+Jefferson023@users.noreply.github.com>
2018-10-31 16:06:11 -0300

    Node done

--- Tree.py
+++ Tree.py
@@ -1,15 +1,29 @@
-class node:
-	def __init__(self, value = None, left = None, right = None):
+import gc
+class Node:
+
+	def __init__(self, value, father = None, right_child = None):
 		self.value = value
-		self.left = left
-		self.right = right
-		
+		self.father = father
+		self.left_child = None	
+		self.right_child = right_child	
+		self.go_left = False
+		gc.enable()
+	def __repr__(self):
+		return self.value
+	def set_left(self, state):
+		self.go_left = state		
+	def get_left(self):
+		return self.go_left
+	def getvalue(self):
+		return self.value
+	def depth(self):
+		counter = 0
+		temp = self.father
+		while (temp is not None):
+			counter += 1
+			temp = temp.father
+		return counter
 
 class binary_tree:
 	def __init__(self):
 		self.tree = []
-	def print(self):
-	
-
-
-		
